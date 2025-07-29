49da6dfbe23932126a18218e5e29f17984751d38
Lucas Lorenzo Savino <savino@MacBook-Air-de-Lucas.local>
2025-07-29 17:21:13 -0300

    configuracão realizada

---
+++ plataforma_mentoria/src/test/java/br/edu/iff/ccc/plataforma_mentoria/PlataformaMentoriaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.iff.ccc.plataforma_mentoria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaMentoriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
